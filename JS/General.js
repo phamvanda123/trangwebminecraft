8fdfdd16b2cd47394fb35915ca3719a112322e7a
phamvanda123 <114846809+phamvanda123@users.noreply.github.com>
2023-12-26 12:48:36 +0700

    commit 1

--- JS/General.js
+++ JS/General.js
@@ -19,3 +19,36 @@ const navinicial = document.querySelector('.nav-logo');
 navinicial.addEventListener('click', () => {
     window.location.href = "../HTML/Index.html";
 });
+load()
+async function load(){
+    const nickname = getCookie('nickname');
+    const apiUrl1 = `http://127.0.0.1:5000/api/getpointfromplayer?name=${nickname}`;
+    try {
+        const responsePoints = await fetch(apiUrl1);
+        const dataPoints = await responsePoints.json();
+        if (dataPoints.points && dataPoints.points.length > 0) {
+            const userPoints = dataPoints.points[0][0];
+            const usernameSpan = document.getElementById('point').textContent ="Point: " +userPoints;
+            
+            
+        }
+    } catch (error) {
+        
+    }
+    const usernameSpan = document.getElementById('Username');
+    if (nickname) {
+        usernameSpan.textContent = nickname;
+    } else {
+        usernameSpan.textContent = 'Login'; // Default text if the cookie is not present
+    }
+}
+function getCookie(name) {
+    const cookies = document.cookie.split(';');
+    for (const cookie of cookies) {
+        const [cookieName, cookieValue] = cookie.trim().split('=');
+        if (cookieName === name) {
+            return decodeURIComponent(cookieValue);
+        }
+    }
+    return null; // Cookie not found
+}
--- JS/Login.js
+++ JS/Login.js
@@ -13,61 +13,70 @@ if(isUserLogged()) {
  
 const form = document.querySelector("#login-form");
 
-form.addEventListener("submit", function (event) {
-	event.preventDefault();
-	const nickInput = document.querySelector("#nick");
-	const confirmInput = document.querySelector("#confirm");
- 
-	const nick = nickInput.value.trim();
-	const confirm = confirmInput.value.trim();
- 
-	if(nick.length < 3 || nick.length > 16) {
-	   form.reset();
-	   nickInput.classList.add("input-invalid");
-	   nickInput.setCustomValidity("Add a nickname between 3 to 16 characters");
-	   nickInput.reportValidity();
- 
-	   setTimeout(() => {
-		  nickInput.classList.remove("input-invalid");
-		  nickInput.setCustomValidity("");
-		}, 2000);
-	   return;
-	}
- 
-	if(confirm.length < 3 || confirm.length > 16) {
-		form.reset();
-		confirmInput.classList.add("input-invalid");
-		confirmInput.setCustomValidity("Add a nickname between 3 to 16 characters");
-		confirmInput.reportValidity();
-  
-		setTimeout(() => {
-			confirmInput.classList.remove("input-invalid");
-			confirmInput.setCustomValidity("");
-		}, 2000);
-		return;
-	}
- 
-	if(nick !== confirm) {
-		form.reset();
-		confirmInput.classList.add("input-invalid");
-		confirmInput.setCustomValidity("The values ​​must be the same.");
-		confirmInput.reportValidity();
-  
-		setTimeout(() => {
-			confirmInput.classList.remove("input-invalid");
-			confirmInput.setCustomValidity("");
-		}, 2000);
-		return;
-	}
- 
-	const date = new Date();
-	date.setTime(date.getTime() + 12 * 60 * 60 * 1000);
-	const expires = ";expires=" + date.toUTCString() + ";path=/"; 
- 
-	document.cookie = "nickname=" + nick + expires;
-	document.cookie = "logged=true" + expires;
- 
-	nickInput.value = "";
-	confirmInput.value = "";
-	window.location.href = "../HTML/Cart.html";
+form.addEventListener("submit", async function (event) {
+    event.preventDefault();
+    const nickInput = document.querySelector("#nick");
+    const nick = nickInput.value.trim();
+    if (nick.length === 0 || nick.length > 16) {
+        form.reset();
+        nickInput.classList.add("input-invalid");
+        nickInput.setCustomValidity("Add a nickname between 1 to 16 characters");
+        nickInput.reportValidity();
+        setTimeout(() => {
+            nickInput.classList.remove("input-invalid");
+            nickInput.setCustomValidity("");
+        }, 2000);
+        return;
+    }
+    const apiUrl = `http://127.0.0.1:5000/api/checkplayer?name=${nick}`;
+	const apiUrl1 = `http://127.0.0.1:5000/api/getpointfromplayer?name=${nick}`;
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        if (data.exists === 1) {
+			try {
+				const responsePoints = await fetch(apiUrl1);
+				const dataPoints = await responsePoints.json();
+				if (dataPoints.points && dataPoints.points.length > 0) {
+					const userPoints = dataPoints.points[0][0];
+
+					// Do something with userPoints if needed
+					console.log('User Points:', userPoints);
+				}
+			} catch (error) {
+				
+			}
+            // Player exists, set cookies and redirect
+            const date = new Date();
+            date.setTime(date.getTime() + 12 * 60 * 60 * 1000);
+            const expires = ";expires=" + date.toUTCString() + ";path=/";
+
+            document.cookie = "nickname=" + nick + expires;
+            document.cookie = "logged=true" + expires;
+
+            nickInput.value = "";
+
+            // Redirect to your desired page
+            window.location.href = "../HTML/Cart.html";
+			
+        } else {
+            // Player doesn't exist, show error message
+            handleInvalidInput(nickInput, "Username does not exist");
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        handleInvalidInput(nickInput, "Error checking username");
+    }
 });
+
+function handleInvalidInput(inputElement, errorMessage) {
+    form.reset();
+    inputElement.classList.add("input-invalid");
+    inputElement.setCustomValidity(errorMessage);
+    inputElement.reportValidity();
+
+    setTimeout(() => {
+        inputElement.classList.remove("input-invalid");
+        inputElement.setCustomValidity("");
+    }, 2000);
+}
